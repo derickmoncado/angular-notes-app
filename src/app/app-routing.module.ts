6db283ac04b5961e0939a31de4f34dd71f19d779
Derick Moncado <derickmoncado@gmail.com>
2021-05-17 14:05:28 -0700

    Static build of form for note-details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteDetailsComponent } from './pages/note-details/note-details.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    children: [{ path: '', component: NotesListComponent }],
+    children: [
+      { path: '', component: NotesListComponent },
+      { path: ':id', component: NoteDetailsComponent },
+    ],
   },
 ];
 
